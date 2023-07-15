31b2492384cb4b437a564a76dc1675b06185f33f
mark-kibo <kibochamark@gmail.com>
2023-07-15 16:17:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+let newHeader=document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+let newCOntent=document.createTextNode("Mark is the champion");
+newHeader.appendChild(newCOntent);
+document.body.appendChild(newHeader);
+
+
